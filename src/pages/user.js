a9f7dd2a3c6528e1237f518bd77e6090da7e53f6
aziz-codes <azizisoffline@gmail.com>
2022-11-05 21:52:02 +0500

    added random users to user page using faker.js

--- src/pages/user.js
+++ src/pages/user.js
@@ -4,7 +4,7 @@ const User = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    const suggestions = [...Array].map((_, i) => ({
+    const suggestions = [...Array(20)].map((_, i) => ({
       username: faker.internet.userName(),
       email: faker.internet.email(),
       avatar: faker.internet.avatar(),
@@ -12,12 +12,39 @@ const User = () => {
     }));
     setUsers(suggestions);
   }, []);
+
   return (
     <div className="m-3 flex flex-col flex-wrap">
       <div>
         <h3 className="font-bold text-xl text-sky-500 tracking-tight">
           Our Happy Clients
         </h3>
+      </div>
+      <div className="flex flex-row w-full flex-wrap mt-4 gap-4 mx-auto">
+        {users.map((user, index) => (
+          <div
+            key={index}
+            className="h-48 w-48 bg-white shadow-xl rounded-sm flex flex-col
+            text-center items-center justify-center border-1"
+          >
+            <img
+              src={user.avatar}
+              alt="avatar"
+              className="w-24 h-24 rounded-full p-1
+             border"
+            />
+            <label className="font-semibold text-md">{user.username}</label>
+            <p
+              className="text-gray-300 p-1 truncat w-full overflow-clip
+             whitespace-nowrap text-sm"
+            >
+              Joined on
+            </p>
+            <p className="font-semibold tracking-tighter">
+              {user.registerdAt.toString().slice(0, 15)}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
