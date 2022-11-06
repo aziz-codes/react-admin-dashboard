05dd2ba4ec39d13853385d8012e9b619a52cd60d
aziz-codes <azizisoffline@gmail.com>
2022-11-06 22:17:24 +0500

    conditionally rendering of content on small screen and large screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import BottomNavigationBar from "./components/BottomNavigationBar";
+// import { useStateContext } from "./contexts/ContextProvider";
 import {
   User,
   Financial,
@@ -13,23 +14,89 @@ import {
 } from "./pages";
 
 const App = () => {
-  const activeMenu = true;
-  return (
-    <BrowserRouter>
-      <div>
-        <BottomNavigationBar />
-        <Routes>
-          {/* Dashboard */}
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [isSmallScreen, setSmallScreen] = useState(false);
 
-          <Route path="/" element={<Projects />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/progress" element={<Progress />} />
-          <Route path="/financial" element={<Financial />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/community" element={<Community />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 768) {
+      setSmallScreen(true);
+    } else {
+      setSmallScreen(false);
+    }
+  }, [screenSize]);
+
+  // const {screenSize,setScreenSize} = useStateContext();
+  const activeMenu = true;
+
+  return (
+    <div>
+      {isSmallScreen ? (
+        <BrowserRouter>
+          <div>
+            <BottomNavigationBar />
+            <Routes>
+              <Route path="/" element={<Projects />} />
+              <Route path="/user" element={<User />} />
+              <Route path="/progress" element={<Progress />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/community" element={<Community />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      ) : (
+        <div>
+          <BrowserRouter>
+            <div className="flex relative dark:bg-main-dark-bg">
+              {activeMenu ? (
+                <div
+                  className="w-64 fixed sidebar
+             dark:bg-secondary-dark-bg
+            bg-white shadow-xl"
+                >
+                  <Sidebar />
+                </div>
+              ) : (
+                <div className="w-0 dark:bg-secondary-dark-bg">
+                  <Sidebar />
+                </div>
+              )}
+              <div
+                className={`dark:bg-main-bg bg-main-bg ml-72 flex-2 flex-wrap
+                min-h-screen w-full ${activeMenu ? " md:ml-72" : "flex-2"}`}
+              >
+                <div className="md:static bg-main-bg dark:bg-main-bg navbar w-full">
+                  <Navbar />
+                </div>
+
+                <div>
+                  <Routes>
+                    {/* Dashboard */}
+
+                    <Route path="/" element={<Projects />} />
+                    <Route path="/user" element={<User />} />
+                    <Route path="/progress" element={<Progress />} />
+                    <Route path="/financial" element={<Financial />} />
+                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/community" element={<Community />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          </BrowserRouter>
+        </div>
+      )}
+    </div>
   );
 };
 export default App;
