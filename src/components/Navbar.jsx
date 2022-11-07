f080b2a822fae93734e25d41bcbb7d7a100e3787
aziz-codes <azizisoffline@gmail.com>
2022-11-07 22:55:18 +0500

    Login and Logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { Bars4Icon } from "@heroicons/react/24/outline";
 import Avatar from "@mui/material/Avatar";
+import { useAuth0 } from "@auth0/auth0-react";
 const Navbar = () => {
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
   return (
     <div className="bg-white flex justify-between h-14 p-2">
       <Bars4Icon className="icons" />
-      <Avatar alt="Remy Sharp" src="" sx={{ width: 30, height: 30 }} />
+      {isAuthenticated ? (
+        <Avatar
+          alt={user.preferred_username}
+          src={user.picture}
+          sx={{ width: 30, height: 30, cursor: "pointer" }}
+          onClick={() => logout({ returnTo: window.location.origin })}
+        />
+      ) : (
+        <Avatar
+          alt="Remy Sharp"
+          src=""
+          sx={{ width: 30, height: 30, cursor: "pointer" }}
+          onClick={() => {
+            loginWithRedirect();
+          }}
+        />
+      )}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 const rootElement = document.getElementById("root");
 
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <Auth0Provider
+    domain="dev-atjv83weiubjlwli.us.auth0.com"
+    clientId="rNLE69Xp4zOgQuGo6QF4ICAdYSuzfqft"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
+);
